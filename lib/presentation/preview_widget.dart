439fbb1b8b026d583071589e69b529d2b855e794
Tasnim Hmida <tasnim.hmida@horizon-tech.tn>
2024-10-22 10:30:32 +0100

    adding hover functionality

--- lib/presentation/preview_widget.dart
+++ lib/presentation/preview_widget.dart
@@ -3,7 +3,7 @@ import 'package:app_builder/utils/wrap_with_parent.dart';
 import 'package:flutter/material.dart';
 import 'component.dart'; // Component class from your existing file
 
-class PreviewWidget extends StatelessWidget {
+class PreviewWidget extends StatefulWidget {
   final double selectedHeight;
   final double selectedWidth;
   final Color selectedColor;
@@ -26,76 +26,96 @@ class PreviewWidget extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<PreviewWidget> createState() => _PreviewWidgetState();
+}
+
+class _PreviewWidgetState extends State<PreviewWidget> {
+  // Store the hover state for each component using a map.
+  final Map<Component, bool> _hoverStates = {};
+
+  @override
   Widget build(BuildContext context) {
     return Expanded(
       child: GestureDetector(
         onTap: () {
-          resetSelection();
+          widget.resetSelection();
         },
         child: Center(
           child: Container(
-            height: selectedHeight,
-            width: selectedWidth,
-            color: selectedColor,
+            height: widget.selectedHeight,
+            width: widget.selectedWidth,
+            color: widget.selectedColor,
             child: DragTarget<String>(
               onAccept: (data) {
-                if (draggableItems.isNotEmpty) {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: const Text('Choose Layout'),
-                        content: const Text('How would you like to arrange the elements?'),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              final existingComponent = draggableItems.last;
-                              final newComponent = createComponent(data, elementColor, onTapElement);
-                              draggableItems.removeLast();
-                              draggableItems.add(wrapWithRow(
-                                firstComponent: existingComponent.child,
-                                secondComponent: newComponent,
-                              ));
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text('Row'),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              final existingComponent = draggableItems.last;
-                              final newComponent = createComponent(data, elementColor, onTapElement);
-                              draggableItems.removeLast();
-                              draggableItems.add(wrapWithColumn(
-                                firstComponent: existingComponent.child,
-                                secondComponent: newComponent,
-                              ));
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text('Column'),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              final existingComponent = draggableItems.last;
-                              final newComponent = createComponent(data, elementColor, onTapElement);
-                              draggableItems.removeLast();
-                              draggableItems.add(wrapWithStack(
-                                firstComponent: existingComponent.child,
-                                secondComponent: newComponent,
-                              ));
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text('Stack'),
-                          ),
-                        ],
-                      );
-                    },
-                  );
+                if (widget.draggableItems.isNotEmpty) {
+                  var existingComponent = widget.draggableItems.last;
+
+                  // Check if the existing component is a layout
+                  if (existingComponent.isLayout) {
+                    final newComponent = createComponent(data, widget.elementColor, widget.onTapElement);
+                    existingComponent = wrapWithParent(
+                      existingComponent: existingComponent.child,
+                      newComponent: newComponent,
+                    );
+
+                    setState(() {
+                      widget.draggableItems[widget.draggableItems.length - 1] = existingComponent;
+                    });
+                  } else {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text('Choose Layout'),
+                          content: const Text('How would you like to arrange the elements?'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                final newComponent = createComponent(data, widget.elementColor, widget.onTapElement);
+                                widget.draggableItems.removeLast();
+                                widget.draggableItems.add(wrapWithRow(
+                                  firstComponent: existingComponent.child,
+                                  secondComponent: newComponent,
+                                ));
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Row'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                final newComponent = createComponent(data, widget.elementColor, widget.onTapElement);
+                                widget.draggableItems.removeLast();
+                                widget.draggableItems.add(wrapWithColumn(
+                                  firstComponent: existingComponent.child,
+                                  secondComponent: newComponent,
+                                ));
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Column'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                final newComponent = createComponent(data, widget.elementColor, widget.onTapElement);
+                                widget.draggableItems.removeLast();
+                                widget.draggableItems.add(wrapWithStack(
+                                  firstComponent: existingComponent.child,
+                                  secondComponent: newComponent,
+                                ));
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Stack'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
                 } else {
-                  addElement(data);
+                  widget.addElement(data);
                 }
               },
               builder: (context, candidateData, rejectedData) {
-                if (draggableItems.isEmpty) {
+                if (widget.draggableItems.isEmpty) {
                   return const Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -109,15 +129,37 @@ class PreviewWidget extends StatelessWidget {
                   );
                 }
                 return Stack(
-                  children: draggableItems.map((component) {
+                  children: widget.draggableItems.map((component) {
+                    // Initialize the hover state if it doesn't exist.
+                    _hoverStates.putIfAbsent(component, () => false);
+
                     return Positioned(
                       left: 20.0,
                       top: 20.0,
-                      child: GestureDetector(
-                        onTap: () {
-                          onTapElement(component.child.toString()); // Assuming it represents the type
+                      child: MouseRegion(
+                        onEnter: (_) {
+                          setState(() {
+                            _hoverStates[component] = true;
+                          });
                         },
-                        child: buildComponent(component),
+                        onExit: (_) {
+                          setState(() {
+                            _hoverStates[component] = false;
+                          });
+                        },
+                        child: GestureDetector(
+                          onTap: () {
+                            widget.onTapElement(component.child.toString());
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: _hoverStates[component] == true
+                                  ? Border.all(color: Colors.blueAccent, width: 2)
+                                  : null,
+                            ),
+                            child: buildComponent(component),
+                          ),
+                        ),
                       ),
                     );
                   }).toList(),
